705f9f1edd34c18f1f6c7ca0b33c0380f69a446d
Stef van Lieshout <stefvanlieshout@fastmail.fm>
2019-09-27 13:40:13 +0200

    DEV-920 Changed bam into cram in sambamba cmd

--- cluster/src/main/java/com/hartwig/pipeline/alignment/vm/SambambaViewCommand.java
+++ cluster/src/main/java/com/hartwig/pipeline/alignment/vm/SambambaViewCommand.java
@@ -3,6 +3,6 @@ package com.hartwig.pipeline.alignment.vm;
 class SambambaViewCommand extends SambambaCommand {
 
     SambambaViewCommand() {
-        super("view", "-f", "bam", "-S", "-l0", "/dev/stdin");
+        super("view", "-f", "cram", "-S", "-l0", "/dev/stdin");
     }
 }
